0a16b91d639ade2b6516a878a275057d8967bfb9
aalexandrams@gmail.com <aalexandrams@gmail.com>
2023-06-28 12:43:03 +0200

    Added missing args.

--- org/diceresearch/nebula/veracity_detection/run_train.py
+++ org/diceresearch/nebula/veracity_detection/run_train.py
@@ -9,7 +9,7 @@ from org.diceresearch.nebula.data.dataset import StanceDataset
 from org.diceresearch.nebula.veracity_detection.model import MLP
 
 """
-    Temporary training script for the first step
+    Temporary training script for the first step of a JSON Lines file.
 """
 
 
@@ -22,6 +22,10 @@ def parse_args():
     parser.add_argument('--file', required=True, help='Path to JSONL file to read from')
     parser.add_argument('--save', default='resources/model.pt', help='Path where to save the trained model')
     parser.add_argument('--top-k', default=10, type=int, help='Top k evidence')
+    parser.add_argument('--dropout', default=0.5, type=float, help='Dropout rate')
+    parser.add_argument('--epochs', default=10, type=int, help='Number of epochs')
+    parser.add_argument('--batch-size', default=512, type=int, help='Batch size')
+    parser.add_argument('--learning-rate', default=1e-4, type=float, help='Learning rate')
     return parser.parse_args()
 
 
@@ -36,7 +40,7 @@ def main():
 
     # convert to Dataset and to DataLoader
     train_dataset = StanceDataset(data, args.top_k)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=512, shuffle=True, drop_last=True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
 
     # create model
     model = MLP(in_channels=args.top_k, hidden_channels=[5, 1],
@@ -45,14 +49,14 @@ def main():
                 norm_layer=torch.nn.BatchNorm1d,
                 activation_layer=torch.nn.ReLU,
                 activation_output=torch.nn.Tanh,
-                bias=True, dropout=0.5)
+                bias=True, dropout=args.dropout)
     logging.info(model)
 
     # train
     train_losses, _ = model.train_model(loss_function=torch.nn.HingeEmbeddingLoss,
                                                  optimizer=torch.optim.Adam,
                                                  training_loader=train_loader,
-                                                 epochs=10, lr=1e-4)
+                                                 epochs=args.epochs, lr=args.learning_rate)
 
     # save model to file
     logging.info('Saved model to {0}'.format(args.save))
