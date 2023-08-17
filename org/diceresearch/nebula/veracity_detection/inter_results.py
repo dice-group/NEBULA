c0381042b1c293e27248d27fb29b0eb60ea41b09
aalexandrasilva <aalexandrams@gmail.com>
2023-08-17 16:17:33 +0200

    Added the source labels to the saved file.

--- org/diceresearch/nebula/veracity_detection/inter_results.py
+++ org/diceresearch/nebula/veracity_detection/inter_results.py
@@ -2,7 +2,12 @@ import argparse
 import glob
 import json
 import logging
+import sys
 import time
+from datetime import timedelta
+from pathlib import Path
+
+sys.path.insert(1, '../')
 from logging.config import fileConfig
 
 import pandas as pd
@@ -21,7 +26,8 @@ def parse_args():
     parser.add_argument('--endpoint', help='Service endpoint')
     parser.add_argument('--path', help='Dataset folder')
     parser.add_argument('--save', help='Path where to save the results')
-    # parser.add_argument('--save-fails', help='Path where to save the failed ids')
+    parser.add_argument('--labels', help='Path where the source labels are')
+    parser.add_argument('--save-fails', help='Path where to save the failed ids')
     return parser.parse_args()
 
 
@@ -33,16 +39,22 @@ STATUS_URL = args.endpoint + "/rawstatus?id="
 def main():
     fileConfig(settings.logging_config)
 
+    source_labels = pd.read_csv(args.labels, header=0, index_col=0)['label'].to_dict()
+
     count = 0
     # read all from folder
+    start = time.time()
     for file in glob.glob(args.path + '/*'):
         print(file)
-        with open(file) as fin:
+        cur_label=source_labels[str(Path(file).stem)]
+        with open(file) as fin, open(args.save, 'a+', encoding='utf8') as fail_save:
             data = json.load(fin)
             for item in data:
                 # submit request to check?lang=en&text=
+                count += 1
+                logging.info('Processed {0} articles'.format(count))
                 article_text = item['content']
-                if article_text is None:
+                if not article_text:
                     continue
                 check_text = CHECK_URL + article_text
 
@@ -57,17 +69,30 @@ def main():
                         time.sleep(1)
                         status_id = check_status(id)
 
+                    if status_id['status'] != 'DONE':
+                        f_result = ResponseStatus(
+                            id=count,
+                            request_id=status_id['id'],
+                            article_id=item['id'],
+                            status=status_id['status'])
+                        fail_save.write('{0}\n'.format(f_result.get_json()))
+
                     with open(args.save, 'a+', encoding='utf8') as f:
+                        res = pd.read_json(status_id['wiseone'], orient='index')
                         small_result = ResponseStatus(
-                            id=status_id['id'],
-                            wiseone=pd.read_json(status_id['wiseone'], orient='index')['wise_score'].to_json(orient='values'),
+                            id=count,
+                            article_id= item['id'],
+                            stancescore=res['stance_score'].to_json(orient='values'),
+                            wiseone=res['wise_score'].to_json(orient='values'),
+                            label=cur_label,
                             status=status_id['status'])
                         f.write('{0}\n'.format(small_result.get_json()))
 
-                    count += 1
-                    if count % 100 == 0:
-                        logging.info('Processed {0} articles'.format(count))
-
+                    if count == 100:
+                        # logging.info('Processed {0} articles'.format(count))
+                        break # FIXME later. Just want 100 articles for testing atm
+    elapsed = (time.time() - start)
+    logging.info('Took {0} for {1} articles'.format(timedelta(seconds=elapsed), count))
 
 def check_status(id):
     response = requests.get(STATUS_URL + id).json()
